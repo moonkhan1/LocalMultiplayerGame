381e4c104bab421a9fd64f5e16e5d56df15f013b
moonkhan1 <ayxanhesenov3105@gmail.com>
2023-02-20 01:05:37 +0400

    Enemy created and navmesh agent added

---
+++ Assets/GameFolder/Scripts/Concretes/Controller/EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityProject3.Controllers
+{ 
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
