bf0bf4a452bd8fa1178d83aab1c9ddd44ed17d4b
moonkhan1 <ayxanhesenov3105@gmail.com>
2023-03-06 19:04:58 +0300

    Menu scene mechanics

--- Assets/GameFolder/Scripts/Concretes/Managers/GameManager.cs
+++ Assets/GameFolder/Scripts/Concretes/Managers/GameManager.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 using UnityProject3.Abstracts.Helpers;
 using UnityProject3.Controllers;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace UnityProject3.Managers
 {
     public class GameManager : SingletonBase<GameManager>
     {
+
         void Awake() 
         {
             MakeSingleton(this);
+        }
+
+        public void LoadScene(string name)
+        {
+            StartCoroutine(LoadSceneAsync(name));
+        }
+
+        private IEnumerator LoadSceneAsync(string name)
+        {
+            yield return SceneManager.LoadSceneAsync(name);
         }
     }
 }
---
+++ Assets/GameFolder/Scripts/Concretes/UIs/StartButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityProject3.Managers;
+using System;
+
+namespace UnityProject3.UIs
+{
+    public class StartButton : MonoBehaviour
+    {
+        Button _button;
+
+        void Awake() 
+        {
+            _button = GetComponent<Button>();    
+        }
+
+        void OnEnable() {
+            _button.onClick.AddListener(HandleOnButtonClicked);
+        }
+
+        void OnDisable() {
+            _button.onClick.RemoveListener(HandleOnButtonClicked);    
+        }
+
+        private void HandleOnButtonClicked()
+        {
+            GameManager.Instance.LoadScene("Game");
+        }
+    }
+}
