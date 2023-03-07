e00880985c0b0bde7f785e66a3f6936f006da50f
moonkhan1 <ayxanhesenov3105@gmail.com>
2023-03-07 22:50:41 +0300

    Full joystick integration added

--- Assets/GameFolder/Scripts/Concretes/Controller/InventoryController.cs
+++ Assets/GameFolder/Scripts/Concretes/Controller/InventoryController.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
+// using UnityEditor.Animations;
 using UnityEngine;
 
 namespace UnityProject3.Controllers
